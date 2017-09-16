a198a4a3ef4045354c665d7e67e95575275d6e4b
Martin Passard <martin.passard@etudiant.univ-lille1.fr>
2017-09-16 15:11:27 +0200

    feat: prepare the motor for pull MVC model

--- src/main/MainObserver.java
+++ src/main/MainObserver.java
@@ -6,30 +6,37 @@ import javafx.stage.Stage;
 import moteur.Observer;
 
 public class MainObserver extends Observer{
-	
+
 	private TextArea t=new TextArea();
 	Stage newFenetre=new Stage();
-	
-	public void lunch(MainSubject ms){
-	
-	t.setEditable(false);
-	t.setText(ms.getState());
-	Scene sceneFenetre=new Scene(t);
-	newFenetre.setScene(sceneFenetre);
-	newFenetre.setTitle("Auditeur");
-	newFenetre.setOnCloseRequest(e->{
-		ms.detach(this);
-	});
-	newFenetre.show();
-	}
 
+	public void lunch(MainSubject ms){
+
+		t.setEditable(false);
+		t.setText(ms.getState());
+		Scene sceneFenetre=new Scene(t);
+		newFenetre.setScene(sceneFenetre);
+		newFenetre.setTitle("Auditeur");
+		newFenetre.setOnCloseRequest(e->{
+			ms.detach(this);
+		});
+		newFenetre.show();
+	}
+	//PUSH VERSION
 	@Override
 	public void update(String message) {
 		System.out.println("Updating: "+message);
 		t.setText(t.getText()+"\n"+message);
-		
+
+	}
+
+	//PULL VERSION (NOT USED HERE)
+	@Override
+	public void update() {
 	}
 	
+	
+	//close
 	public void close(){
 		newFenetre.close();
 		try {
@@ -39,5 +46,7 @@ public class MainObserver extends Observer{
 			e.printStackTrace();
 		}
 	}
+
+
 
 }
--- src/moteur/Observer.java
+++ src/moteur/Observer.java
@@ -4,5 +4,7 @@ public abstract class Observer {
 	
 public abstract void update(String message);
 
+public abstract void update();
+
 public abstract void close();
 }
